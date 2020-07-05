68b45ccb7635c3653be09e9393ae97b8e575a44d
Adriaan Knapen <hi@aknapen.nl>
2020-07-05 12:31:16 +0200

    feat(gatsby): adds siteMetadata configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  siteMetadata: {
+    title: "Adriaan Knapen",
+    description: "Personal website of Adriaan Knapen",
+    locale: "en",
+  },
   plugins: [
     {
       resolve: "@wkocjan/gatsby-theme-intro",
